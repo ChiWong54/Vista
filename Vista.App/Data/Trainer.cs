61eacc87e467f1910ec0ccd114df47019f568c92
ChiWong54 <chifd28@hotmail.com>
2024-10-10 12:05:41 +0100

    Done

---
+++ Vista.App/Data/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.App.Data
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        [MaxLength(50)]
+        public string? Location { get; set; } // The question mark allow nulls
+        public List<TrainerCategory>? TrainerCategories { get; set; } // Placeholder for List of Trainer Categories (many side of one-to-many)
+        public List<Session>? Sessions { get; set; } // Placeholder for List of Sessions (many side of one-to-many)
+    }
+}
---
+++ Vista.App/Data/TrainerCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.App.Data
+{
+    public class TrainerCategory
+    {
+        // Has a composite (compound) key that will be defined TrainersDbConext
+        [Required]
+        public int TrainerId { get; set; }
+        [MaxLength(15)]
+        public required string CategoryCode { get; set; }
+        public Trainer? Trainer { get; set; } // Placeholder a navigation property to Trainer (one side of one-to-many)
+        // See TrainersDbConext for Foreign Key (Fluent API) definition
+        public Category? Category { get; set; } // Placeholder for a navigation property to Category (one side of one-to-many)
+    }
+}
